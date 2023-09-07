973da078e0eca7ef38e8434c99068ed6b7131573
David Barbee <dbarbee@pcienergysolutions.com>
2023-09-07 18:55:39 -0500

    Initial commit

---
+++ src/main/java/com/deejtech/cap/omass/OperationalMissionAlertingSupportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.deejtech.cap.omass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationalMissionAlertingSupportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationalMissionAlertingSupportSystemApplication.class, args);
+	}
+
+}
